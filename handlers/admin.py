0351415dbb9e04ee381fb19877d0263f95989fc2
Igor <igor9.i@yandex.ru>
2022-06-25 13:04:27 +0300

    problem state

--- handlers/admin.py
+++ handlers/admin.py
@@ -9,8 +9,10 @@ class FSMAdmin(StatesGroup):
     description = State()
     price = State()
 
+
 #@dp.message_handler(commands='Загрузить', state = None)
 async def cm_start(message: types.Message):
+    print("dfdscsd  {}".format(message.from_user.id))
     await FSMAdmin.photo.set()
     await message.reply('Загрузи фото')
 
@@ -43,9 +45,20 @@ async def load_price(message: types.Message, state: FSMContext):
         await message.reply(str(data))
     await state.finish()
 
+@dp.message_handler(state='*', commands='отмена')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await  state.get_state()
+    if current_state is None:
+        return
+    await  state.finish()
+    await  message.reply('OK')
+
+
+
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(cm_start, commands=['Загрузить'],state=None)
     dp.register_message_handler(load_photo, content_types= ['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
+    dp.register_message_handler(cancel_handler, commands = 'отмена')
