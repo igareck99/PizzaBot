b8f5c4b83ea65b4c3ab7fbf8b7fca6b1ad23d049
Igor <igor9.i@yandex.ru>
2022-06-25 09:54:53 +0300

    second

--- bot_telegram.py
+++ bot_telegram.py
@@ -2,8 +2,23 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-bot = Bot(token = '5181943973:AAGwCFyK7nwaox8NsTn8ZT0SUciA7VXXypI')
+bot = Bot(token ='5181943973:AAGwCFyK7nwaox8NsTn8ZT0SUciA7VXXypI')
 dp = Dispatcher(bot)
+
+async def on_startup(_):
+    print('Бот успешно запущен')
+
+@dp.message_handler(commands=['start', 'help'])
+async def commands_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через лс, напишит ему')
+
+@dp.message_handler(commands=['Режим_работы'])
+async def commands_start(message: types.Message):
+    await bot.send_message(message.from_user.id, 'ПН-ВС 10:00 - 23:00')
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
@@ -13,4 +28,4 @@ async def echo_send(message: types.Message):
     # await bot.send_message(message.from_user.id, message.text)
 
 
-executor.start_polling(dp, skip_updates = True)
+executor.start_polling(dp, skip_updates = True, on_startup =  on_startup)
