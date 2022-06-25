56958c6145377e0a1cdf36e5ff9a0821bdca074f
Igor <igor9.i@yandex.ru>
2022-06-25 12:43:22 +0300

    create working state machine

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,6 +6,7 @@ async def on_startup(_):
     print('Бот успешно запущен')
 
 client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates = True, on_startup =  on_startup)
--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 
 bot = Bot(token ='5181943973:AAGwCFyK7nwaox8NsTn8ZT0SUciA7VXXypI')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
